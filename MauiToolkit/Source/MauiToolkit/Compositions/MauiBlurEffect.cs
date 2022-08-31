0cb781eb6ee33a3f8a94043487bd55c04d2d8209
吴锋 <wufeng4728704@hotmail.com>
2022-08-31 23:45:44 +0800

    add MauiBlurEffect

---
+++ MauiToolkit/Source/MauiToolkit/Compositions/MauiBlurEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiToolkit.Compositions;
+public partial class MauiBlurEffect
+{
+    public static readonly BindableProperty BlurEffectProperty =
+                       BindableProperty.CreateAttached("BlurEffect", typeof(MauiBlurEffect), typeof(MauiBlurEffect), default, propertyChanged: OnBlurEffectPropertyChanged);
+
+    public static MauiBlurEffect GetBlurEffect(VisualElement target) => (MauiBlurEffect)target.GetValue(BlurEffectProperty);
+    public static void SetBlurEffect(VisualElement target, MauiBlurEffect value) => target.SetValue(BlurEffectProperty, value);
+
+    private static void OnBlurEffectPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        throw new NotImplementedException();
+    }
+}
